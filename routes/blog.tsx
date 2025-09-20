d6d8b5f54596e1499cc16c9dcc0ac1ce5b811537
たすくん <nizi.minato@gmail.com>
2025-09-20 09:50:33 +0900

    [update]: 記事一覧取得方法の更新

--- routes/blog.tsx
+++ routes/blog.tsx
@@ -1,19 +1,42 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
-import { parseFeed, Feed } from "rss/mod.ts";
 
 import Header from "../components/Header.tsx";
 import Footer from "../components/Footer.tsx";
 
-import ArticleCards from "../components/ArticleCards.tsx";
-
 import { NavigationMenu } from "../utils.ts";
 
 interface Data {
-	feed: Feed
+	items: any;
 };
 
 export const handler: Handlers<Data> = {
 	async GET(_req, ctx) {
+		{
+			const headers = {
+				Authorization: "Bearer " + Deno.env.get("QIITA_API_KEY")
+			};
+
+			const response = await fetch(`https://qiita.com/api/v2/users/23tas9/items`, {
+				headers: headers
+			});
+
+			const json = await response.json();
+
+			for(const item of json){
+				console.log(item.title);
+			}
+
+			console.log(json[0]);
+
+			const item = json[0];
+			item.created_at;
+			item.id;
+			item.tags;
+			item.title;
+			item.user.name;
+
+			return ctx.render({ items: json });
+		}
 
 		const response = await fetch("https://qiita.com/23tas9/feed");
 		const xml = await response.text();
@@ -24,8 +47,6 @@ export const handler: Handlers<Data> = {
 };
 
 export default function Home({ data }: PageProps<Data>) {
-	const feed: Feed = data.feed;
-
 	const menus: NavigationMenu[] = [
 		{
 			title: "Home",
@@ -40,7 +61,26 @@ export default function Home({ data }: PageProps<Data>) {
 	return (
 		<div className="wrapper">
 			<Header menus={menus} />
-			<ArticleCards title="ブログ" feed={feed} />
+				<article className="article-cards grid-container">
+				{data.items.map(item=>(
+					<section className="article-cards__item">
+						<a href={`/blog/${item.id}`}>
+							<h3 className="article-cards__item__title">{item.title}</h3>
+							<p>{
+								item.updated_at?.toLocaleString("ja-jp", {
+									year: "numeric",
+									month: "2-digit",
+									day: "2-digit"
+								}).replaceAll('/', '-')
+							}</p>
+							<p>
+								執筆者:
+								{item.user.name}
+							</p>
+						</a>
+					</section>
+				))}
+				</article>
 			<Footer />
 		</div>
 	);
